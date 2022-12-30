1a54f8ddfde9ff5381b2b08685f043f893d391ff
nastya-koptilova <koptilova.nastya@gmail.com>
2022-12-30 16:59:17 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+});
+
+const onPlay = function (data) {
+  const { seconds } = data;
+  try {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const onPause = () => {
+  try {
+    const stopTime = localStorage.getItem('videoplayer-current-time');
+
+    return stopTime === null ? undefined : JSON.parse(stopTime);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.setCurrentTime(onPause());
+
+player.on('timeupdate', throttle(onPlay, 1000));
